73fac6092b650b998f5567c0e2a020cf45d77edc
wany82075 <wany82075@163.com>
2020-03-28 16:40:09 +0800

    commit again

---
+++ src/main/java/cn/dlut/edu/data/ApplicationContextHelper.java
@@ -0,0 +1,23 @@
+package cn.dlut.edu.data;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextHelper implements ApplicationContextAware {
+	private static ApplicationContext context;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		context = applicationContext;
+	}
+
+	public static <T> T getBean(Class<T> tClass) {
+		if (null == context) {
+			return null;
+		}
+		return context.getBean(tClass);
+	}
+}
